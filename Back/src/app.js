0626816806c0ce6345767ddbcc5a7a322e8d3099
Leonardo <lareyes.24@est.ucab.edu.ve>
2025-11-08 18:55:36 -0400

    Inicie el back de crear cuenta, falta solucionar errores con el servidor

--- Back/src/app.js
+++ Back/src/app.js
@@ -1,12 +1,18 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
-const routes = require("./routes/index");
-const authRoutes = require("./routes/authRoutes");
 
-app.use(express.json()); // para leer JSON en las peticiones
+app.use(express.json());
+
+const routes = require('./routes/index');
+const authRoutes = require('./routes/authRoutes');
+
+app.use('/', routes);                // rutas generales
+app.use('/auth', authRoutes);        // todas las rutas de auth vivirán en /auth/*
+
+// servir la página principal
+app.get('/crear-cuenta', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', '..', 'Front', 'public', 'crearCuenta.html'));
+});
 
 module.exports = app;
-
-app.use("/", routes);
-
-app.use("/auth", authRoutes);
